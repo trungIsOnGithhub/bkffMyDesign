ae99a0cae1da5499f9a242dcca709c0ad8720f1b
Trung Nguyen <trung150502@gmail.com>
2024-05-06 14:51:54 +0000

    define callable template and import to dag

--- dag/dag.py
+++ dag/dag.py
@@ -1,7 +1,12 @@
 import DAG from airflow
+import sys
 import datetime from datetime
 import PythonOperator from airflow.operators.python
 
+# make this module available for import
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+import extract_data_from_source_callable, transform_data_format_callable, write_data_to_sink_callable from pipelines.pipeline
 
 tasks_dag = DAG(
     dag_id = 'sample_data_pipeline_flow',
@@ -15,7 +20,7 @@ tasks_dag = DAG(
 
 extract_data_from_source = PythonOperator(
     task_id = "extract_data_from_source",
-    python_callable = "extract_data_from_source",
+    python_callable = extract_data_from_source_callable,
     provide_context=True,
     op_args = ["https://en.wikipedia.org/wiki/List_of_association_football_stadiums_by_capacity"],
     dag = tasks_dag
@@ -23,14 +28,14 @@ extract_data_from_source = PythonOperator(
 
 transform_data_format = PythonOperator(
     task_id = "transform_data_format",
-    python_callable = "",
+    python_callable = transform_data_format_callable,
     provide_context=True,
     dag = tasks_dag
 )
 
 write_data_to_sink = PythonOperator(
     task_id = "write_data_to_sink",
-    python_callable = "write_data_to_sink",
+    python_callable = write_data_to_sink_callable,
     provide_context=True,
     dag = tasks_dag
 )
---
+++ pipelines/pipelines.py
@@ -0,0 +1,5 @@
+def extract_data_from_source(**args):
+
+def transform_data_format():
+
+def write_data_to_sink():
